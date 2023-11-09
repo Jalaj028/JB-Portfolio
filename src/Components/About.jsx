4549c1e51338492ea7e4ac65fa10f387f62bf5aa
Jalaj Bhatt <117558817+Jalaj028@users.noreply.github.com>
2023-11-09 22:41:58 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a b.tech undergrad studying at Birla Institute Of Applied Sciences, Bhimtal. I enjoy learning new tech stacks, currently I am learning MERN stack.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
+
+ * Mernstack | Java | Open Source
+ * I am passionate about contributing to open source repositories and building real world projects
+ 
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Mongodb",
+  "Javascript",
+  "Nodejs",
+  "React",
+  "Express",
+  "Java",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By actively contributing to opensouce repositories i try to sharpen my skills.";
 
 const About = () => {
   return (
